6d37c4255df0412906a32b9ed30ef26f474268d8
boojongmin <boojongmin@gmail.com>
2025-09-26 14:43:33 +0900

    update

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@ rm -rf ../kta-portal/{docker,admin,portal,school,api}
 cp -r docker ../kta-portal/docker
 cp -r admin ../kta-portal/admin
 cp -r api ../kta-portal/api
-cp -r util ../kta-portal/util
+cp -r util ../kta-portal/
 cp .gitignore ../kta-portal/
 mkdir -p ../kta-portal/docs/example/frontend
 cp -r docs/example/frontend ../kta-portal/docs/example/frontend
